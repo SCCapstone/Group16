4d0141c0258d3d1ee496111da87f8fcd6beeec42
Mikul958 <michaelpikula958@gmail.com>
2024-11-15 19:07:26 -0500

    Generated GradeCalculatorComponent

---
+++ src/fe-app/src/app/grades/grade-calculator/grade-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './grade-calculator.component.html',
+  styleUrl: './grade-calculator.component.css'
+})
+export class GradeCalculatorComponent {
+
+}
