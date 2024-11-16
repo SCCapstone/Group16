cc51182b9fd1642c1e1a7cf9fdd238a6a7cdeaaa
Mikul958 <michaelpikula958@gmail.com>
2024-11-15 19:39:15 -0500

    Added skeleton for GradeCalculatorComponent

--- src/fe-app/src/app/grades/grade-calculator/grade-calculator.component.ts
+++ src/fe-app/src/app/grades/grade-calculator/grade-calculator.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './grade-calculator.component.css'
 })
 export class GradeCalculatorComponent {
+  
+  // Undefined temporary until Grade and Course types are added
+  realGrades: undefined;
+  temporaryGrades: undefined;
+  selectedCourse: undefined;
 
+  addCategory(category: string) {}
+  addGrade(category: string, grade: number) {}
+  calculateFinalGrade() {}
 }
