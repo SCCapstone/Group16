1692237c5a64e50add89fac8a1969cac4eebc60e
Mikul958 <michaelpikula958@gmail.com>
2024-11-18 17:45:44 -0500

    Added routing info to routes.ts

--- src/fe-app/src/app/routes.ts
+++ src/fe-app/src/app/routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { SettingsComponent } from './settings/settings.component';
+import { GradesComponent } from './grades/grades.component';
 
 const routeConfig: Routes = [
   {
@@ -12,6 +16,21 @@ const routeConfig: Routes = [
     path: 'login',
     component: LoginComponent,
     title: 'Login'
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+    title: 'Main'
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    title: 'Settings'
+  },
+  {
+    path: 'grades',
+    component: GradesComponent,
+    title: 'Grades'
   }
 ];
 
