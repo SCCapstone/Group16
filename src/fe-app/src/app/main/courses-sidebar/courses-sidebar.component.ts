f8873d2b0dfa62b9aced4c75195720832fe402f0
fschein <fschein02@gmail.com>
2024-11-26 16:53:19 -0500

    created course service

--- src/fe-app/src/app/main/courses-sidebar/courses-sidebar.component.ts
+++ src/fe-app/src/app/main/courses-sidebar/courses-sidebar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Course } from '../../course';
+import { CourseService } from '../../course.service';
+import { LoginService } from '../../login.service';
 
 @Component({
   selector: 'app-courses-sidebar',
@@ -8,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrl: './courses-sidebar.component.css'
 })
 export class CoursesSidebarComponent {
+  test: Course[] = [];
+  courseService = inject(CourseService);
+  loginService = inject(LoginService);
 
+  getCourseTester(): void {
+    this.courseService.getCourses(this.loginService.getUserId())
+    .then((courses: Course[]) => {
+      this.test = courses;
+      console.log(courses[0].name)
+    })
+  }
 }
