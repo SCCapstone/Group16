ac7650ccbd8b6ee95535e084607725435cff0d99
Mikul958 <michaelpikula958@gmail.com>
2024-11-26 16:49:59 -0500

    Set up template form in AddTaskComponent and added routing to add-task from main page

--- src/fe-app/src/app/main/add-task/add-task.component.ts
+++ src/fe-app/src/app/main/add-task/add-task.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
   name: string = "";
   type: string = "";
-  course: undefined; // undefined temporary until type Course is created
-  due: Date = new Date();
+  course: string = "";
+  // course: undefined; // TODO update type to Course when interface is created
+  due: string = "";
+  // due: Date = new Date();
+
+  displayOutput: boolean = false;  // TODO for testing, remove later
 
   constructor() {}
-  addTask() {}
+  addTask() {
+    this.displayOutput = true;
+
+    // TODO Validate date and send new task to course service
+  }
 }
