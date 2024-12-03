d5d04effbe4a9f13019a4a9fb8682dc3002bf1f3
Mikul958 <michaelpikula958@gmail.com>
2024-12-02 22:04:30 -0500

    Added selected course ID and associated methods to course service

--- src/fe-app/src/app/course.service.ts
+++ src/fe-app/src/app/course.service.ts
@@ -7,6 +7,8 @@ import { Course } from './course';
 export class CourseService {
   url = 'http://104.234.231.191:1616/api/getCourses';
 
+  private selectedCourse: string = "";    // ID of course selected in CoursesSidebarComponent
+
   constructor() { }
 
   async getCourses(userId: string | null) : Promise<Course[]> {
@@ -14,5 +16,23 @@ export class CourseService {
     const data = await response.json() ?? [];
     console.log(data);
     return data;
+  }
+
+  // Returns the ID of the currently-selected course
+  getSelectedCourse(): string {
+    return this.selectedCourse;
+  }
+
+  // Updates the currently-selected course ID
+  selectCourse(courseID: string): void {
+    if (courseID === this.selectedCourse)
+      this.selectedCourse = "";
+    else
+      this.selectedCourse = courseID;
+  }
+
+  // Clears course selection; used while initializing a page
+  deselectCourse(): void {
+    this.selectedCourse = "";
   }
 }
