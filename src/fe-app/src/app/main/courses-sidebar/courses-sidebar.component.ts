a0384b42eb2b253c5e9bf868fc5aceb8e24f5a51
Mikul958 <michaelpikula958@gmail.com>
2024-12-02 22:13:48 -0500

    Now stores selected course in CourseService as course ID

--- src/fe-app/src/app/main/courses-sidebar/courses-sidebar.component.ts
+++ src/fe-app/src/app/main/courses-sidebar/courses-sidebar.component.ts
@@ -15,7 +15,7 @@ export class CoursesSidebarComponent {
   loginService = inject(LoginService);
 
   courses: Course[] = [];
-  selectIndex: Number = -1;   // Index of course selected by user; -1 indicates none.
+  // selectIndex: Number = -1;   // Index of course selected by user; -1 indicates none.
 
   // Retrieves course list from course service and stores it locally
   constructor() {
@@ -23,20 +23,37 @@ export class CoursesSidebarComponent {
     .then((courses: Course[]) => {
       this.courses = courses;
     })
+
+    this.courseService.deselectCourse();  // Ensure no course is selected on page load
   }
 
   // Updates selectIndex to clicked-on course or deselects it if already selected
+  selectCourse(courseID: string): void {
+    if (courseID === this.courseService.getSelectedCourse())
+      this.courseService.deselectCourse();
+    else
+      this.courseService.selectCourse(courseID);
+  }
+  /*
   selectCourse(index: Number): void {
     if (index === this.selectIndex)
       this.selectIndex = -1;
     else
       this.selectIndex = index;
   }
+  */
 
   // Returns CSS class for the given index based on whether or not it is selected
+  getStyle(courseID: string): string {
+    if (courseID === this.courseService.getSelectedCourse())
+      return "course selected";
+    return "course";
+  }
+  /*
   getStyle(index: Number): string {
     if (index === this.selectIndex)
       return "course selected";     // Element in question will use both "course" and "selected" CSS classes
     return "course";
   }
+  */
 }
