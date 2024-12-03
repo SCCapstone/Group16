12d313892719fbcd943be489e96ea428734e12ca
fschein <fschein02@gmail.com>
2024-12-02 20:58:39 -0500

    completed assignment service

---
+++ src/fe-app/src/app/assignment.ts
@@ -0,0 +1,7 @@
+export interface Assignment {
+  id: string,
+  userId: string,
+  courseId: string,
+  title: string,
+  description: string
+}
--- src/fe-app/src/app/main/main.component.ts
+++ src/fe-app/src/app/main/main.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 import { LoginService } from '../login.service';
 import { CoursesSidebarComponent } from "../main/courses-sidebar/courses-sidebar.component";
 import { DueSoonSidebarComponent } from '../main/due-soon-sidebar/due-soon-sidebar.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 const VIEW_CALENDAR: number = 0;
@@ -10,11 +11,11 @@ const VIEW_TASK_LIST: number = 1;
 const VIEW_NOTIFICATIONS: number = 2; // This will not be necessary if I can get notification overlay working
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [RouterOutlet, RouterModule, CoursesSidebarComponent, DueSoonSidebarComponent],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [RouterOutlet, RouterModule, CoursesSidebarComponent, DueSoonSidebarComponent, TaskListComponent]
 })
 export class MainComponent {
   loginService = inject(LoginService);
@@ -26,7 +27,7 @@ export class MainComponent {
       this.output = this.loginService.getUserId();
     }
   }
-  
-  // Will be refactored with consts when I figure out how; 0 = calendar, 1 = task list, 2 = 
+
+  // Will be refactored with consts when I figure out how; 0 = calendar, 1 = task list, 2 =
   viewSelect: number = 0;
 }
