ba360421e8708f2d0296c625cb55727534d87103
Mikul958 <michaelpikula958@gmail.com>
2025-01-17 22:49:00 -0500

    Add reactive form to profile settings

--- src/fe-app/src/app/settings/profile-settings/profile-settings.component.ts
+++ src/fe-app/src/app/settings/profile-settings/profile-settings.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile-settings.component.html',
   styleUrl: './profile-settings.component.css'
 })
@@ -13,5 +15,39 @@ export class ProfileSettingsComponent {
   personalEmail: string = "";
   phoneNumber: string = "";
 
-  updateSettings() {}
+  // TODO trying to load 
+  profileForm = new FormGroup({
+    name: new FormControl(),
+    school: new FormControl(),
+    personal: new FormControl(),
+    phone: new FormControl()
+  })
+
+  constructor() {
+    // TODO load variables from settings service when it is implemented, these are temporary
+    this.preferredName = "Michael"
+    this.schoolEmail = "michael@sc.edu"
+    this.personalEmail = "michael@gmail.com"
+    this.phoneNumber = "555-555-5555"
+
+    // TODO this sucks figure out if there's a better way
+    this.profileForm.setValue({
+      name: this.preferredName,
+      school: this.schoolEmail,
+      personal: this.personalEmail,
+      phone: this.phoneNumber
+    })
+  }
+
+  saveProfile() {
+    // TODO validate properly
+    this.preferredName = this.profileForm.value.name ?? "";
+    this.schoolEmail = this.profileForm.value.school ?? "";
+    this.personalEmail = this.profileForm.value.personal ?? "";
+    this.phoneNumber = this.profileForm.value.phone ?? "";
+
+    // TODO call course service with new info
+
+    alert(this.preferredName + " | " + this.schoolEmail + " | " + this.personalEmail + " | " + this.phoneNumber);
+  }
 }
