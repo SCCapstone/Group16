a8a7d3f90d54f9c712192a11013aa22c94adccdd
Mikul958 <michaelpikula958@gmail.com>
2025-01-17 21:26:43 -0500

    Import all necessary settings components

--- src/fe-app/src/app/settings/settings.component.ts
+++ src/fe-app/src/app/settings/settings.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
+import { SettingsSidebarComponent } from './settings-sidebar/settings-sidebar.component';
+import { AppearanceSettingsComponent } from './appearance-settings/appearance-settings.component';
+import { NotificationSettingsComponent } from './notification-settings/notification-settings.component';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
+
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    SettingsSidebarComponent,
+    AppearanceSettingsComponent,
+    NotificationSettingsComponent,
+    ProfileSettingsComponent,
+    SignOutComponent
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
