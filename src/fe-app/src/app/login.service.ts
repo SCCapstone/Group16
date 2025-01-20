0ad82d6c9c2f9aef831df96e381195864b27789e
fschein <fschein02@gmail.com>
2025-01-20 14:50:30 -0500

    userId no longer clears on refresh, but is cleared when application is closed

--- src/fe-app/src/app/login.service.ts
+++ src/fe-app/src/app/login.service.ts
@@ -8,22 +8,22 @@ export class LoginService {
   url = 'https://classmate.osterholt.us/api/login';
   // url = 'localhost:1616/api/login';
 
-  private userId: string | null = null; // stores userId for use in other components
+  private readonly USER_ID_KEY = 'userId'; // stores userId for use in other components
 
   constructor() { }
 
   async login(username: string, password: string) : Promise<User> {
     const response = await fetch(`${this.url}?username=${username}&password=${password}`);
     const user: Promise<User> = await response.json() ?? {};
-    this.userId = (await user).id || null;
+    sessionStorage.setItem(this.USER_ID_KEY, (await user).id);
     return user;
   }
 
   getUserId(): string | null {
-    return this.userId;
+    return sessionStorage.getItem(this.USER_ID_KEY);
   }
 
   signout(): void { // to be used in signout later
-    this.userId = null;
+    sessionStorage.removeItem(this.USER_ID_KEY);
   }
 }
