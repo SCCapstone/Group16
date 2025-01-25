a4c9d7dfbda01e54d31e690ad7e422957ea71e1c
Mikul958 <michaelpikula958@gmail.com>
2025-01-25 01:14:55 -0500

    Added functionality to sign out button

--- src/fe-app/src/app/settings/sign-out/sign-out.component.ts
+++ src/fe-app/src/app/settings/sign-out/sign-out.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { LoginService } from '../../login.service';
 
 @Component({
   selector: 'app-sign-out',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sign-out.component.html',
   styleUrl: './sign-out.component.css'
 })
-export class SignOutComponent {
-  signOut() {
-    alert("TODO: IMPLEMENT SIGNOUT");
+export class SignOutComponent
+{
+  loginService = inject(LoginService);
+
+  signOut(){
+    this.loginService.signout();
   }
 }
