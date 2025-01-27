543c82c16673dfed9b7ac9518a05cbf1ee7a4430
GarterScales <garterscales619@yahoo.com>
2025-01-26 20:59:05 -0500

    behavioral test login

--- src/fe-app/tests/example_behavioral_test.spec.ts
+++ src/fe-app/tests/example_behavioral_test.spec.ts
@@ -10,3 +10,14 @@ test('example_behavioral_test', async ({ page }) => {
   await page.getByRole('button', { name: 'Login', exact: true }).click();
   await expect(page.getByText('Login failed, please try again')).toBeVisible();
 });
+
+test('test_login', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('button', { name: 'Go To Login' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).fill('osterholt');
+  await page.getByRole('textbox', { name: 'Password:' }).click();
+  await page.getByRole('textbox', { name: 'Password:' }).fill('cameron1234');
+  await page.getByRole('button', { name: 'Login', exact: true }).click();
+  await expect(page).toHaveURL(/\/main$/);
+});
