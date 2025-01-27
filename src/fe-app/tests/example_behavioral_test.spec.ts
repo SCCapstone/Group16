1a0bd839179c17f7ed3154dde26aa134954c5f3d
GarterScales <garterscales619@yahoo.com>
2025-01-26 20:51:07 -0500

    Using playwright

---
+++ src/fe-app/tests/example_behavioral_test.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('example_behavioral_test', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('button', { name: 'Go To Login' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).click();
+  await page.getByRole('textbox', { name: 'Username:' }).fill('osterholt');
+  await page.getByRole('textbox', { name: 'Password:' }).click();
+  await page.getByRole('textbox', { name: 'Password:' }).fill('wrongpassword');
+  await page.getByRole('button', { name: 'Login', exact: true }).click();
+  await expect(page.getByText('Login failed, please try again')).toBeVisible();
+});
