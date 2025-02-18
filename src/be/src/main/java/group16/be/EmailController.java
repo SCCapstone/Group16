2bc62918954454e66d6d8eb5abcec307e5517923
Cayden Scruggs <garterscales619@yahoo.com>
2025-02-17 21:33:32 -0500

    sends emails

--- src/be/src/main/java/group16/be/EmailController.java
+++ src/be/src/main/java/group16/be/EmailController.java
@@ -2,7 +2,13 @@ package group16.be;
 
 import java.util.Properties;
 
-import jakarta.mail.*;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Multipart;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeBodyPart;
@@ -16,21 +22,28 @@ public class EmailController {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "sandbox.smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
+        prop.put("mail.smtp.host", "smtp.mailersend.net");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.ssl.trust", "smtp.mailersend.net");
         Session session = Session.getInstance(prop, new Authenticator() {
+
+/**
+ * Provides the username and password for email authentication.
+ *
+ * @return a PasswordAuthentication object containing the email username and password
+ */
+
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("d011365dd72bf7", "18322f0fe4c681");
+                return new PasswordAuthentication("MS_VlGYll@trial-zr6ke4nd9y9gon12.mlsender.net", "mssp.uEACt7v.v69oxl5y10k4785k.edHBwr3");
             }
         });
 
 
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("from@gmail.com"));
+        message.setFrom(new InternetAddress("from@trial-zr6ke4nd9y9gon12.mlsender.net"));
         message.setRecipients(
-        Message.RecipientType.TO, InternetAddress.parse("fschein02@gmail.com"));
+        Message.RecipientType.TO, InternetAddress.parse("scruggscayden@gmail.com"));
         message.setSubject("Mail Subject");
 
         String msg = "This is my first email using JavaMailer";
