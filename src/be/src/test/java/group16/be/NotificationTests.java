527d184d438b359ab3d26b4da8b294f0d0d40783
Cam Osterholt <github@osterholt.us>
2025-03-05 21:07:28 -0500

    Moved notification tests to their own file.

---
+++ src/be/src/test/java/group16/be/NotificationTests.java
@@ -0,0 +1,60 @@
+package group16.be;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import group16.be.db.GradeRepository;
+
+public class NotificationTests {
+    private final String MONGO_URI = System.getenv("MONGO_URI");
+    private final String[] COLLECTIONS = { "assignments", "courses", "users", "grades" };
+
+	/* Listener Test Variables */
+	private final String REAL_USERID = "673fdd30cc2da4c3a3514fb7";
+	private final String REAL_GRADEID = "679d3e875a709c3bbfdb83a3";
+
+	@Autowired
+	private APIScraper scraper;
+
+	@Autowired
+	private MongoChangeListener mongoChangeListener;
+
+	@Autowired
+	private NotificationManager notificationManager;
+    
+    @Test
+	void testNotifications() {
+		var user = scraper.getUser(REAL_USERID);
+		assertTrue(user.getNotifications() == null);
+
+		notificationManager.sendNotification(user, "Test Notification 1");
+
+		assertTrue(user.getNotifications().size() > 0);
+		System.out.println("Notification: " + user.getNotifications().peek());
+	}
+
+	@Test
+	void listenerTests() throws InterruptedException {
+		assertTrue(scraper.getUser(REAL_USERID).getNotifications().size() == 0);
+		mongoChangeListener.init();
+
+		// I am going to modify a grade of "osterholt" in the database. This should update the notifications.
+		var user = scraper.getUser(REAL_USERID);
+
+		var grade = scraper.getGradeByGradeId(REAL_GRADEID);
+		grade.setPercent(91);
+		scraper.saveGrade(grade);
+		grade.setPercent(90);
+		scraper.saveGrade(grade);		
+
+		Thread.sleep(500);
+		user = scraper.getUser(REAL_USERID);
+		
+		assertTrue(user.getNotifications().size() > 0);
+		System.out.println("Notification: " + user.getNotifications().peek());
+
+		notificationManager.clearNotifications(REAL_USERID);
+	}
+}
