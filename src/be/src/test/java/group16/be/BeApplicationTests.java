385e55a971be992527613e7b14cc253c108a77d4
Cam Osterholt <github@osterholt.us>
2025-03-05 21:07:47 -0500

    Refactor BeApplicationTests by removing unused imports

--- src/be/src/test/java/group16/be/BeApplicationTests.java
+++ src/be/src/test/java/group16/be/BeApplicationTests.java
@@ -1,15 +1,7 @@
 package group16.be;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.bson.Document;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.mongodb.client.MongoCollection;
-
-import group16.be.db.GradeRepository;
 
 @SpringBootTest(classes = BeApplication.class)
 public class BeApplicationTests {
