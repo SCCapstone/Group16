54c37537f2f90525f58694ee6fe04ad271abfc40
Cam Osterholt <github@osterholt.us>
2025-03-16 15:29:47 -0400

    Update minikube command to use static IP for improved network configuration

--- .k8s/build_k8s.bash
+++ .k8s/build_k8s.bash
@@ -1,5 +1,5 @@
 cd /home/cam/capstone/Group16
-minikube start
+minikube start --static-ip 192.168.49.2
 docker login ghcr.io -u osterholt -p $GITHUB_AUTH
 kubectl apply -f .k8s/classmate-api.yaml
 kubectl apply -f .k8s/classmate-fe.yaml
