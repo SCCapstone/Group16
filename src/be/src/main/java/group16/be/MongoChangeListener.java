fc58915b46ff562304233ba619095b95c4e23839
Cam Osterholt <github@osterholt.us>
2025-03-27 16:29:43 -0400

    emergency remove ChangeListener

--- src/be/src/main/java/group16/be/MongoChangeListener.java
+++ src/be/src/main/java/group16/be/MongoChangeListener.java
@@ -12,38 +12,38 @@ import jakarta.annotation.PostConstruct;
 
 @Service
 public class MongoChangeListener {
-    private final String MONGO_URI = System.getenv("MONGO_URI");
-    private final String[] COLLECTIONS = { "assignments", "courses", "users", "grades" };
+    // private final String MONGO_URI = System.getenv("MONGO_URI");
+    // private final String[] COLLECTIONS = { "assignments", "courses", "users", "grades" };
 
-    private MongoClient mongoClient;
+    // private MongoClient mongoClient;
 
-    @Autowired
-    private NotificationManager notificationManager;
+    // @Autowired
+    // private NotificationManager notificationManager;
 
-    // TODO: Change COLLECTIONS from static reference.
-    // TODO: Send to NotificationManager.
-    @PostConstruct
-    public void init() {
-        mongoClient = MongoClients.create(MONGO_URI);
-        new Thread(() -> watchCollection(COLLECTIONS[0])).start(); // assignments
-        new Thread(() -> watchCollection(COLLECTIONS[1])).start(); // courses
-        new Thread(() -> watchCollection(COLLECTIONS[2])).start(); // users
-        new Thread(() -> watchCollection(COLLECTIONS[3])).start(); // grades
-    }
+    // // TODO: Change COLLECTIONS from static reference.
+    // // TODO: Send to NotificationManager.
+    // @PostConstruct
+    // public void init() {
+    //     mongoClient = MongoClients.create(MONGO_URI);
+    //     new Thread(() -> watchCollection(COLLECTIONS[0])).start(); // assignments
+    //     new Thread(() -> watchCollection(COLLECTIONS[1])).start(); // courses
+    //     new Thread(() -> watchCollection(COLLECTIONS[2])).start(); // users
+    //     new Thread(() -> watchCollection(COLLECTIONS[3])).start(); // grades
+    // }
 
-    private void watchCollection(String collectionName) {
-        MongoCollection<Document> collection = getCollection(collectionName);
+    // private void watchCollection(String collectionName) {
+    //     MongoCollection<Document> collection = getCollection(collectionName);
         
-        var changeStream = collection.watch().iterator();
-        while (changeStream.hasNext()) {
-            var change = changeStream.next();
-            // System.out.println("DEBUG: MONGO CHANGE" + change);
-            notificationManager.parseChange(change);
-        }
-    }
+    //     var changeStream = collection.watch().iterator();
+    //     while (changeStream.hasNext()) {
+    //         var change = changeStream.next();
+    //         // System.out.println("DEBUG: MONGO CHANGE" + change);
+    //         notificationManager.parseChange(change);
+    //     }
+    // }
 
-    public MongoCollection<Document> getCollection(String collectionName) {
-        var db = mongoClient.getDatabase("bb_data");
-        return db.getCollection(collectionName);
-    }
+    // public MongoCollection<Document> getCollection(String collectionName) {
+    //     var db = mongoClient.getDatabase("bb_data");
+    //     return db.getCollection(collectionName);
+    // }
 }
