114ab46f19daba28cacfb3e65e160ec2b7e3997f
GarterScales <garterscales619@yahoo.com>
2025-04-06 17:17:53 -0400

    Test

--- src/fe-app/tests/example_behavioral_test.spec.ts
+++ src/fe-app/tests/example_behavioral_test.spec.ts
@@ -21,3 +21,34 @@ test('test_login', async ({ page }) => {
   await page.getByRole('button', { name: 'Login', exact: true }).click();
   await expect(page).toHaveURL(/\/main$/);
 });
+
+test('test_click_around', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('button', { name: 'Go To Login' }).click();
+  await page.getByRole('button', { name: 'Auto-Login' }).click();
+  await page.locator('app-task').filter({ hasText: 'CSCE546Mar 24 MeetingMar 31,' }).getByRole('checkbox').check();
+  await page.locator('app-task').filter({ hasText: 'CSCE546Mar 24 MeetingMar 31,' }).getByRole('checkbox').uncheck();
+  await page.getByRole('link', { name: 'calendar icon' }).click();
+  await page.getByText('THURSDAY').click();
+  await page.getByRole('link', { name: 'grades icon' }).click();
+  await page.getByText('COURSE').click();
+  await page.getByRole('img', { name: 'add task icon' }).click();
+  await page.getByText('Title:').click();
+  await page.getByText('×').click();
+  await page.getByRole('img', { name: 'calculator icon' }).click();
+  await page.getByRole('cell', { name: 'Assignment Label' }).click();
+  await page.getByRole('button', { name: 'Reset All' }).click();
+  await page.getByText('×').click();
+  await page.getByRole('link', { name: 'settings icon' }).click();
+  await page.getByText('Preferred Name:').click();
+  await page.getByText('Appearance').click();
+  await page.getByText('Notifications').click();
+  await page.getByText('Text Messages').click();
+  await page.getByRole('checkbox', { name: 'Text Messages' }).uncheck();
+  await page.getByText('Sign Out').click();
+  await page.getByRole('button', { name: 'Back to Main Page' }).click();
+  await page.locator('header a').nth(1).click();
+  await page.getByText('×').click();
+  await page.getByRole('heading', { name: 'Due Soon' }).click();
+  await page.getByRole('img', { name: 'classmate icon' }).click();
+});
