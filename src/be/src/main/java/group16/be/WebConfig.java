554b94de2c8497e4b70f102b816f9fb3e196c5b4
Cam Osterholt <github@osterholt.us>
2025-04-06 13:22:53 -0400

    Implement Backend Cookies

---
+++ src/be/src/main/java/group16/be/WebConfig.java
@@ -0,0 +1,17 @@
+package group16.be;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("classmate.osterholt.us")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
+    }
+}
