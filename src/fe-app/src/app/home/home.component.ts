8bf725b932659ca85dcdc8d6427bae83fc13c4ea
avaturner <avart@email.sc.edu>
2025-04-05 22:07:52 -0400

    small styling fixes

--- src/fe-app/src/app/home/home.component.ts
+++ src/fe-app/src/app/home/home.component.ts
@@ -11,20 +11,16 @@ import { LoginService } from '../login.service';
 })
 export class HomeComponent {
   router = inject(Router);
-  title = 'Home Page'; // Assume this will be used appropriately in future styling
-  currentValue: string = ''; // is this needed?
+  title = 'Home Page';
+  currentValue: string = '';
   loginService = inject(LoginService);
 
   constructor(public activatedRoute: ActivatedRoute) {}
 
-  // we can go back to routerLink in the html, but I am yet to find a good way to test it
-  // we likely can expect the framework to handle routerLink properly, but discussion will be needed
-  // router.navigate does do a sufficient job as well
   click() {
     this.router.navigate(['/login']);
     console.log('Button clicked');
   }
-
 
 }
 
