8b6456d4f2e8580584e15b09511e6cb766cbdc1c
GarterScales <garterscales619@yahoo.com>
2025-04-27 14:44:11 -0400

    no longer report errors

--- src/be/src/main/java/group16/be/DataSyncScheduler.java
+++ src/be/src/main/java/group16/be/DataSyncScheduler.java
@@ -1,7 +1,5 @@
 package group16.be;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,14 +12,6 @@ public class DataSyncScheduler {
 
     @Scheduled(fixedRate = 3600000) // Every hour
     public void scheduledValidation() {
-        List<String> errors = dataSyncModule.validateDatabase();
-        if (!errors.isEmpty()) {
-            System.out.println("Scheduled Data Validation Errors:");
-            for (String error : errors) {
-                System.out.println(error);
-            }
-        } else {
-            System.out.println("Scheduled data validation passed. No issues found.");
-        }
+        dataSyncModule.cleanInvalidDatabaseEntries();
     }
 }
